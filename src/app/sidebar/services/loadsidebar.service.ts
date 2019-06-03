fd149549935e807ee60deb30f44b0d08e8a90f86
Xavier Johnson <xavierjohnson@Xavier-MacBook-Pro.local>
2019-06-03 16:04:16 -0400

    Routing test

--- src/app/sidebar/services/loadsidebar.service.ts
+++ src/app/sidebar/services/loadsidebar.service.ts
@@ -5,7 +5,7 @@ export const Sidebars: Sidebar[] = [
   {id: 2, name: 'Manage Assets', visible: true, link: '/manageassets', menu: 'mainsub-a', mainmenu: 'assets'},
   {id: 2.1, name: 'Lists', visible: false, link: '/manageassets/lists', menu: 'sub-A', mainmenu: 'assets'},
   {id: 2.2, name: 'Assets', visible: false, link: '/manageassets/assets', menu: 'sub-A', mainmenu: 'assets'},
-  {id: 2.3, name: 'Tickets', visible: false, link: '/manageassets/tickets', menu: 'sub-A', mainmenu: 'assets'},
+  {id: 2.3, name: 'Tickets', visible: false, link: 'manageassets/tickets', menu: 'sub-A', mainmenu: 'assets'},
   {id: 3, name: 'Budget' , visible: true, link: '/budgets', menu:  'main', mainmenu: 'budget'},
   {id: 4, name: 'Admin', visible: true, link: '/admin', menu: 'mainsub-b', mainmenu: 'admin'},
   {id: 4.1, name: 'Accounts', visible: false, link: '/admin/accounts', menu: 'sub-B', mainmenu: 'admin'},
