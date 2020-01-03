5db3e32e090a13aa39d52a68a4cc9f1e08f81e8f
Xavier Johnson <xavierjohnson@Xavier-MacBook-Pro.local>
2020-01-03 13:02:26 -0500

    update contract filter

--- src/app/services/apifilter.service.ts
+++ src/app/services/apifilter.service.ts
@@ -6,12 +6,14 @@ import { Profile } from '../login/models/profile';
 import { ProfileService } from '../login/services/profile.service';
 
 import { Filter } from './../models/filter';
-import { Partner } from './../types/partner.enum';
 import { Role } from '../types/role.enum';
 import { Assets } from '../manage-assets/models/assets';
 import { Contracts } from './../manage-assets/models/contracts';
 
 import { HttpClient } from '@angular/common/http';
+
+import { Partner } from '../models/partner';
+import { PartnerList } from '../partner-list';
 
 // import { DataSource } from '@angular/cdk/collections';
 
@@ -22,15 +24,21 @@ import { HttpClient } from '@angular/common/http';
 export class ApifilterService {
   contracts: Contracts;
   profile: Profile;
-
-  partnerList = 'https://prodharmony.azurewebsites.net/api/PartnerList?code=2e6AULJLQxO60bOdJxfX6oxo57jkNueQEn4nsCKixFMjoheKzBc48w==';
+  partnerlist = PartnerList;
+  filteredPartner: Partner;
+  partnerListapi = 'https://prodharmony.azurewebsites.net/api/PartnerList?code=2e6AULJLQxO60bOdJxfX6oxo57jkNueQEn4nsCKixFMjoheKzBc48w==';
 
   customerApi = 'https://prodharmony.azurewebsites.net/api/CompanyByPartner?code=a7VaewYNOND36Oo0A85ezo9m4bvmkQAzZPtOIfp3dPO/SROa2pE/dA==';
 // customersapi =
 // 'https://n1sharmonypull.azurewebsites.net/api/GetCompanies?code=vQsPGD8KR7cHjSP0hENehP9P3v5LKn7eY4JsmO9ALdB4Bc0a99Nmhg==';
 
 
-  contractsapi = 'https://n1sharmonypull.azurewebsites.net/api/ContractsPull?code=exD/Xcz5HraenO9WyxGEyB7HloKBbOO2GJ5Xo1CwVO6T6pv9Q/Fl8A==';
+  pContractsApi =
+  'https://prodharmony.azurewebsites.net/api/ContractsByPartner?code=GCrRyi4Y8rxCQ6mKJzLXYhROqECUR1clXjK9AdqhMEXEdOFPCqK8WA==';
+  cContractsApi =
+  'https://prodharmony.azurewebsites.net/api/ContractsByCustomer?code=SQVrhe8b7rFFsPV0LS6dxBniJW8QqVoa4Jjzv5gwLVuiD/Tg8uIvbQ==';
+
+
   assetsapi = 'https://prodharmony.azurewebsites.net/api/PartnerPullAssets?code=BmCsQrHGSIvMr1oFRw7T6kj7/a2H/x8GeOadWjmvZgSeaRwCqZGE9g==';
 // assetsapi = 'https://n1sharmonypull.azurewebsites.net/api/AssetsPull?code=qQXwJm1YBabl4J8QlK6a2n2/JpY/mTacr66EYRdsZ2i3RfUkAucX4g==';
   ticketsapi = 'https://n1sharmonypull.azurewebsites.net/api/HttpTrigger1?code=lsPvad3uQA6s/pe1imbqoK0egnysVrGlsZXvaFsZ1jc69X6OdKQHcw==';
@@ -69,10 +77,17 @@ export class ApifilterService {
   }
 
   contractsFilter(filter: Filter) {
-    const params = {
-      'partner': filter.partner
-    };
-    return this.http.post(this.contractsapi, params);
+    let i;
+    for (i = 0; i < this.partnerlist.length; i++) {
+      if ( filter.partner === this.partnerlist[i].CompanyName ) {
+        return this.http.post(this.pContractsApi, filter);
+      } else
+      if ( filter.partner !== this.partnerlist[i].CompanyName ) {
+        return this.http.post(this.cContractsApi, filter);
+      } else {
+        console.log('Company: ' + filter.partner + 'not recognized!');
+      }
+    }
   }
 
   // contractByRef(refNumber: string): Observable<Contracts> {
