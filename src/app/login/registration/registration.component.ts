a78c8e7f20a5e9a03d64f76fa1e8ed378dd65de0
Shawn McGee <smcgee@noble1solutions.com>
2020-01-17 14:41:42 -0500

    reg form

--- src/app/login/registration/registration.component.ts
+++ src/app/login/registration/registration.component.ts
@@ -5,6 +5,7 @@ import { first } from 'rxjs/operators';
 import { AlertService } from '../services/alert.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { ProfileService } from '../services/profile.service';
+import { MustMatch } from '../helpers/must-match';
 
 @Component({
   selector: 'app-registration',
@@ -32,13 +33,22 @@ export class RegistrationComponent implements OnInit {
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
       firstName: ['',  Validators.required],
-      lastName: ['', Validators.required],
-      username: ['', Validators.required],
-      partner: ['', Validators.required],
-      email: ['', Validators.email],
+      lastName:  ['', Validators.required],
+      company: ['', Validators.required],
+      companypartner: ['', Validators.required],
+      email:     ['', [Validators.email, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$') ]],
+      partner:   ['', Validators.required],
       partnerRole: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
+      password:  ['', [Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$') ]],
+      confirmpassword: ['', [Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$') ]],
+      username:  ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$') ]]
+    },
+    {
+      validator: [
+         MustMatch('password', 'confirmpassword'),
+         MustMatch('email', 'username')
+      ]
+  });
   }
   // convenience getter for easy access to form fields
   get f() {
