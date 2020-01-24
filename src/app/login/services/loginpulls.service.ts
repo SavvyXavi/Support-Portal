acce5ffc1a63c49321256188637e9f2f391a05a6
Shawn McGee <smcgee@noble1solutions.com>
2020-01-24 17:45:52 -0500

    pass partner variable3

--- src/app/login/services/loginpulls.service.ts
+++ src/app/login/services/loginpulls.service.ts
@@ -1,5 +1,6 @@
+import { Profile } from './../models/profile';
 import { Injectable } from '@angular/core';
-import { Profile } from '../models/profile';
+import { Filter } from '../../models/filter';
 import { Role } from '../../types/role.enum';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
@@ -13,12 +14,13 @@ export class LoginpullsService {
   constructor(
     private http: HttpClient,
   ) { }
-  code = '`${environment.CusLoginPull}CompanyListByPartner?code=rhtQGzt22H6Z0VQb7iUNZYazTiZpKrCmkSEA71oORrDu/lUFysLEoA==`,';
+  code = '${environment.CusLoginPull}CompanyListByPartner?code=rhtQGzt22H6Z0VQb7iUNZYazTiZpKrCmkSEA71oORrDu/lUFysLEoA==';
 
   getPartnerList(): Observable<any> {
     return this.http.get(`${environment.LoginPull}PartnerList?code=6W5az23O1cyKatIJp7F/ayclp8hQal5rYbCywjOXN6kF5ZMzNluuVA==`);
   }
-  getCompanyList(partner: string): Observable<any> {
-    return this.http.get(this.code + partner);
+  getCompanyList(partner): Observable<any> {
+    return this.http.get(this.code, partner);
   }
+
 }
