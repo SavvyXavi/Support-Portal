99bb97ed017aa8ee9681dca78c86b8320fba1617
Shawn McGee <smcgee@noble1solutions.com>
2020-01-24 15:09:35 -0500

    pass partner variable

--- src/app/login/services/loginpulls.service.ts
+++ src/app/login/services/loginpulls.service.ts
@@ -13,11 +13,12 @@ export class LoginpullsService {
   constructor(
     private http: HttpClient,
   ) { }
+  code = '`${environment.CusLoginPull}CompanyListByPartner?code=rhtQGzt22H6Z0VQb7iUNZYazTiZpKrCmkSEA71oORrDu/lUFysLEoA==`,';
 
   getPartnerList(): Observable<any> {
     return this.http.get(`${environment.LoginPull}PartnerList?code=6W5az23O1cyKatIJp7F/ayclp8hQal5rYbCywjOXN6kF5ZMzNluuVA==`);
   }
   getCompanyList(partner: string): Observable<any> {
-    return this.http.get(`${environment.CusLoginPull}CompanyListByPartner?code=rhtQGzt22H6Z0VQb7iUNZYazTiZpKrCmkSEA71oORrDu/lUFysLEoA==`);
+    return this.http.get(this.code + {partner});
   }
 }
