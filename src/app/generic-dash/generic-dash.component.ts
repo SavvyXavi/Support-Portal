8ac2103265e7145d40cec518aff18f1ec38c2cde
Xavier Johnson <xavierjohnson@Xavier-MacBook-Pro.local>
2020-02-05 12:35:31 -0500

    actual grab asset 1

--- src/app/generic-dash/generic-dash.component.ts
+++ src/app/generic-dash/generic-dash.component.ts
@@ -67,6 +67,8 @@ export class GenericDashComponent implements OnInit {
     this.assetsCount();
     this.ticketsCount();
     this.companiesCount();
+
+    this.getAssets();
   }
 
   getPartners() {
@@ -222,13 +224,27 @@ export class GenericDashComponent implements OnInit {
   }
 
   assetsChart() {
-
-    // this.api.getAssets()
+    // if (this.filterPartner(this.filteredProfile.partner)) {
+    //   this.filter.partAssetsFilter(this.filteredProfile)
     //   .subscribe(
     //     (returnedAssets: Assets[]) => {
     //       this.assets = returnedAssets;
+          // this.assetDataSource = new MatTableDataSource(returnedAssets);
+          // this.assetDataSource.sort = this.sort;
+          // this.assetDataSource.paginator = this.paginator;
     //     }
     //   );
+    // } else if (this.filterPartner(this.filteredProfile.partner) === undefined) {
+    //   this.filter.custAssetsFilter(this.filteredProfile)
+    //   .subscribe(
+    //     (returnedAssetLength: Assets[]) => {
+    //       this.assets = returnedAssetLength;
+          // this.assetDataSource = new MatTableDataSource(returnedAssetLength);
+          // this.assetDataSource.sort = this.sort;
+          // this.assetDataSource.paginator = this.paginator;
+    //     }
+    //   );
+    // }
 
       const status = this.assets.map(asset => asset.assetStatus);
       console.log(status);
