204f86909d27c273387ad4981c02437f6a1c966d
Shawn McGee <smcgee@noble1solutions.com>
2020-02-07 11:26:55 -0500

    format of vars

--- src/app/admin/services/oem.service.ts
+++ src/app/admin/services/oem.service.ts
@@ -8,7 +8,7 @@ export class OemService {
   monApi = '?apikey=0R3eFd6iCnPNJUGDgpWY0RAWTfjiGQ4ZskVsbMP4cCoPdi4eYgYo89FWUIqEQJNk&pretty=1';
   oemPull = 'https://n1sharmonypull.azurewebsites.net/api/OEMPull?code=1d9quTWcruk3APk5NqxpPyN8Qez0flIun778ihBgIfB/QKj1eGDMyQ==';
   monPull = 'https://localmonitor.noble1it.com/nagiosxi/api/v1/objects/servicestatus';
-
+  combPull = 'this.monPull + this.monApi';
 
   constructor( private http: HttpClient ) { }
 
@@ -16,6 +16,6 @@ export class OemService {
     return this.http.get(this.oemPull);
   }
   getMon() {
-    return this.http.get(this.monPull + this.monApi);
+    return this.http.get(this.combPull);
   }
 }
