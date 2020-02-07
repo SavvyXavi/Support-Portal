1c58b0030f2305bbbe7b722778bdb5df80f02cb9
Shawn McGee <smcgee@noble1solutions.com>
2020-02-07 12:19:36 -0500

    removed pretty again

--- dist/main-es2015.js
+++ dist/main-es2015.js
@@ -1451,7 +1451,7 @@ __webpack_require__.r(__webpack_exports__);
 let OemService = class OemService {
     constructor(http) {
         this.http = http;
-        this.monApi = '?apikey=0R3eFd6iCnPNJUGDgpWY0RAWTfjiGQ4ZskVsbMP4cCoPdi4eYgYo89FWUIqEQJNk&pretty=1';
+        this.monApi = '?apikey=0R3eFd6iCnPNJUGDgpWY0RAWTfjiGQ4ZskVsbMP4cCoPdi4eYgYo89FWUIqEQJNk';
         this.oemPull = 'https://n1sharmonypull.azurewebsites.net/api/OEMPull?code=1d9quTWcruk3APk5NqxpPyN8Qez0flIun778ihBgIfB/QKj1eGDMyQ==';
         this.monPull = 'https://localmonitor.noble1it.com/nagiosxi/api/v1/objects/servicestatus';
         this.combPull = this.monPull + this.monApi;
