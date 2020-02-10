420228b6f416881b9057a1302edf99f3d2c5117b
Shawn McGee <smcgee@noble1solutions.com>
2020-02-10 09:42:19 -0500

    combo

--- dist/main-es5.js
+++ dist/main-es5.js
@@ -2386,9 +2386,9 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
         _classCallCheck(this, OemService);
 
         this.http = http;
-        this.monApi = '?token=0R3eFd6iCnPNJUGDgpWY0RAWTfjiGQ4ZskVsbMP4cCoPdi4eYgYo89FWUIqEQJNk';
+        this.monApi = 'nagiosxi/api/v1/objects/servicestatus?apikey=0R3eFd6iCnPNJUGDgpWY0RAWTfjiGQ4ZskVsbMP4cCoPdi4eYgYo89FWUIqEQJNk';
         this.oemPull = 'https://n1sharmonypull.azurewebsites.net/api/OEMPull?code=1d9quTWcruk3APk5NqxpPyN8Qez0flIun778ihBgIfB/QKj1eGDMyQ==';
-        this.monPull = 'https://localmonitor.noble1it.com/nagiosxi/api/v1/objects/servicestatus';
+        this.monPull = 'https://localmonitor.noble1it.com/nagiosxi/api/v1/objects/servicestatus?apikey=';
         this.combPull = this.monPull + this.monApi;
       }
 
--- src/app/admin/services/oem.service.ts
+++ src/app/admin/services/oem.service.ts
@@ -5,9 +5,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class OemService {
-  monApi = '?token=0R3eFd6iCnPNJUGDgpWY0RAWTfjiGQ4ZskVsbMP4cCoPdi4eYgYo89FWUIqEQJNk';
+  monApi = 'nagiosxi/api/v1/objects/servicestatus?apikey=0R3eFd6iCnPNJUGDgpWY0RAWTfjiGQ4ZskVsbMP4cCoPdi4eYgYo89FWUIqEQJNk';
   oemPull = 'https://n1sharmonypull.azurewebsites.net/api/OEMPull?code=1d9quTWcruk3APk5NqxpPyN8Qez0flIun778ihBgIfB/QKj1eGDMyQ==';
-  monPull = 'https://localmonitor.noble1it.com/nagiosxi/api/v1/objects/servicestatus';
+  monPull = 'https://localmonitor.noble1it.com/nagiosxi/api/v1/objects/servicestatus?apikey=';
   combPull = this.monPull + this.monApi;
 
   constructor( private http: HttpClient ) { }
