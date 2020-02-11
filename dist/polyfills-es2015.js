5dfcc2d751d188b815495782fe4beb25c79e1c02
Xavier Johnson <xavierjohnson@Xavier-MacBook-Pro.local>
2020-02-11 12:34:36 -0500

    merge 1

--- dist/polyfills-es2015.js
+++ dist/polyfills-es2015.js
@@ -3024,11 +3024,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 module.exports = __webpack_require__(/*! /Users/xavierjohnson/Documents/Support-Portal/src/polyfills.ts */"./src/polyfills.ts");
-=======
-module.exports = __webpack_require__(/*! /Users/shawnmcgee/projects/Support-Portal/src/polyfills.ts */"./src/polyfills.ts");
->>>>>>> origin/newshawn
 
 
 /***/ })
