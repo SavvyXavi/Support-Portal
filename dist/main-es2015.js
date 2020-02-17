0d0b432cc8c8888603572c6ad5c2dc84e0ee32be
Xavier Johnson <xavierjohnson@Xavier-MacBook-Pro.local>
2020-02-17 17:08:42 -0500

    Logo list finished

--- dist/main-es2015.js
+++ dist/main-es2015.js
@@ -5537,6 +5537,99 @@ const LogoList = [
     },
     {
         'CompanyName': 'Relutech', 'Logo': '../assets/RelusLogo.png'
+    },
+    {
+        'CompanyName': 'SPADAFY', 'Logo': '../assets/spadafylogo.jpeg'
+    },
+    {
+        'CompanyName': 'xByte Technologies', 'Logo': '../assets/xByte-20kb-login.jpg'
+    },
+    {
+        'CompanyName': 'Alucid Solutions', 'Logo': '../favicon.ico',
+    },
+    {
+        'CompanyName': 'B2X Global', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Bluestone Network Solutions', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Critical Systems', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'CXTec', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Integrity', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'International Systems Management', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Integrated Service Partners', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'IT Xchange Corp', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Integrated Technical Services (ITSco)', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Northland Systems, Inc', 'Logo': '../assets/newNS_logo.png'
+    },
+    {
+        'CompanyName': 'Procurri', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Riverstone Technology LLC', 'Logo': '../assets/RS-Logo.png'
+    },
+    {
+        'CompanyName': 'Scale Logic', 'Logo': '../assets/SLI-Corp-logo-login.jpg'
+    },
+    {
+        'CompanyName': 'Summit Information Resources', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Sword & Shield - New Name - Avertium', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Verus Global', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Dynamic', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Georgia Computer', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Atlantix Global Systems', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Avnet', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Delta Computer Group', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Anova IT', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Mojo Systems', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'neoRhino IT Solutions', 'Logo': '../favicon.ico',
+    },
+    {
+        'CompanyName': 'Ace IT Technologies, Inc', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'Mega Monolithic Corp', 'Logo': '../assets/Mega Monolithic Corp - Logo Light Small.png'
+    },
+    {
+        'CompanyName': 'Knowledge Computers Inc', 'Logo': '../favicon.ico'
+    },
+    {
+        'CompanyName': 'PB&J Partners LLC', 'Logo': '../favicon.ico'
     }
 ];
 
