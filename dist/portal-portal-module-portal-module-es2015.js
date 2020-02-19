ee1d1c9b0fa5ab18786229e77230abce28796ced
Xavier Johnson <xavierjohnson@Xavier-MacBook-Pro.local>
2020-02-19 12:12:55 -0500

    route edit 2

--- dist/portal-portal-module-portal-module-es2015.js
+++ dist/portal-portal-module-portal-module-es2015.js
@@ -38463,7 +38463,7 @@ function ContractTableComponent_td_15_Template(rf, ctx) { if (rf & 1) {
 } if (rf & 2) {
     const contract_r353 = ctx.$implicit;
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵadvance"](1);
-    _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵpropertyInterpolate"]("routerLink", contract_r353.RefNumber);
+    _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵpropertyInterpolate1"]("routerLink", "/", contract_r353.RefNumber, "");
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵadvance"](1);
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtextInterpolate"](contract_r353.ScheduleName);
 } }
