b18ed8de8a959deddfa87c27960d0a65f8eb2796
Shawn McGee <smcgee@noble1solutions.com>
2020-03-17 09:53:43 -0400

    few more changes 2

--- dist/main-es5.js
+++ dist/main-es5.js
@@ -8502,8 +8502,8 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
                   labels: ['Now', 'Thirty Days', 'Ninety plus'],
                   datasets: [{
                     data: [_this33.now, _this33.thirtyDays, _this33.plus],
-                    backgroundColor: ['rgba(255, 0, 0, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
-                    borderColor: ['rgba(255, 0, 0, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+                    backgroundColor: ['rgba(255, 0, 0, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+                    borderColor: ['rgba(255, 0, 0, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
                     borderWidth: 1
                   }]
                 },
